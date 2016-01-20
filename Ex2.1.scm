98a1511362607946c6c5550909a6cf5a073b5085
yuchien <yc2257@cornell.edu>
2016-01-19 21:11:25 -0500

    Finish Exercise 2.1

---
+++ Ex2.1.scm
@@ -0,0 +1,58 @@
+#lang planet neil/sicp
+
+
+;; Exercise 2.1:
+;; Define a better version of make-rat that handles both positive
+;; and negative arguments. Make-rat should normalize the sign so
+;; that if the rational number is positive, both the numerator and
+;; denominator are positive, and if the rational number is negative,
+;; only the numerator is negative.
+
+
+(define (make-rat n d)
+  (let ([g (gcd n d)]
+        [sign (if (> (* n d) 0) + -)])
+    (cons (sign (abs (/ n g)))
+          (abs (/ d g)))))
+
+(define (numer x) (car x))
+
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+
+;; Testing
+(check-expect (make-rat -1 2) (make-rat -1 2))
+
+(check-expect (make-rat 1 -2) (make-rat -1 2))
+
+(check-expect (make-rat -1 -2) (make-rat 1 2))
+
+(check-expect (make-rat 1 2) (make-rat 1 2))
