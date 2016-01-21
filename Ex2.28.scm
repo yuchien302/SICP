ce050eca8234d9d75f2e651523cf7dfe6f245777
yuchien <yc2257@cornell.edu>
2016-01-21 18:04:27 -0500

    Finish Exercise 2.28

---
+++ Ex2.28.scm
@@ -0,0 +1,32 @@
+#lang planet neil/sicp
+
+;; Exercise 2.28:
+;;
+;; Write a procedure fringe that takes as argument a tree
+;; (represented as a list) and returns a list whose elements are all the
+;; leaves of the tree arranged in left-to-right order. For example,
+;;
+;; (define x 
+;;   (list (list 1 2) (list 3 4)))
+;;
+;; (fringe x)
+;; (1 2 3 4)
+;;
+;; (fringe (list x x))
+;; (1 2 3 4 1 2 3 4)
+
+(define (fringe x)
+  (define (fringe-iter x res)
+    (cond ((null? x) res)
+          ((not (pair? x)) (cons x res))
+          (else
+           (fringe-iter (car x) (fringe-iter (cdr x) res)))))
+  (fringe-iter x '()))
+
+
+
+;; Testing
+(define x 
+   (list (list 1 2) (list 3 4)))
+
+(check-expect (fringe x) '(1 2 3 4))
